5218efe2bd1b0821229ac5d40cba8fc996ed8e69
Kirill <dzerkipl@gmail.com>
2024-12-12 13:02:39 +0100

    Test

--- main/main_js.js
+++ main/main_js.js
@@ -1,24 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll(".slider-image"); // Получаем все изображения в слайдере
-    let currentIndex = 0; // Индекс текущего активного изображения
 
-    function showNextImage() {
-        images[currentIndex].classList.remove("active"); // Убираем класс "active" с текущего изображения
-        currentIndex = (currentIndex + 1) % images.length; // Переходим к следующему изображению (циклично)
-        images[currentIndex].classList.add("active"); // Добавляем класс "active" к следующему изображению
-    }
 
-    images[currentIndex].classList.add("active"); // Показываем первое изображение
-
-    setInterval(showNextImage, 7000); // Меняем изображение каждые 7 секунд
+//Верхняя панель
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("show-panel");
 });
 
 
 
 
+const sliderWrapper = document.querySelector('.slider-wrapper');
+const slides = document.querySelectorAll('.slide');
 
+let currentIndex = 0;
 
+// Функция обновления слайда
+function updateSlider() {
+    sliderWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
 
+// Автоматическое переключение слайдов
+function startAutoSlide() {
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateSlider();
+    }, 3000); // Интервал в 3000 мс (3 секунды)
+}
+
+// Запуск слайдера
+startAutoSlide();
 
 
 
